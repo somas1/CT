f6323bf48aaec2b7ca98c5f9996129cefbaf6147
John Thomas <netflixemailaccount@gmail.com>
2023-12-29 17:49:13 -0500

    Modified by www.SourceFiles.app

--- Shell-Scripting/boot.sh
+++ Shell-Scripting/boot.sh
@@ -3,4 +3,12 @@
 echo "Do you want to play rocks-paper-scissors? [y/n]"
 read -p "Play game? " answer
 
-python3 rock-paper-scissors.py
+case $answer in
+   [yY]* ) 
+       python3 rocks-paper-scissors.py
+       ;;
+   [nN]* ) 
+       echo "Okay, maybe next time."
+       ;;
+   * ) echo "Invalid input, please enter 'y' or 'n'.";;
+esac
