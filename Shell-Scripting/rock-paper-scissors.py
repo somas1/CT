560d492ff49569e414467a47e09ccc8070090fa1
John Thomas <netflixemailaccount@gmail.com>
2023-12-29 16:13:09 -0500

    Modified by www.SourceFiles.app

--- Shell-Scripting/rock-paper-scissors.py
+++ Shell-Scripting/rock-paper-scissors.py
@@ -1,10 +1,19 @@
 import random
 
-def menu():
+def player():
     choices = ["rock", "paper", "scissors"]
     choice = input("Rock, paper or scissors?: ")
 
     if choice not in choices:
         print("Please enter 'rock', 'paper' or 'scissors'")
-        menu()
+        player()
     return choice
+
+def computer_choice():
+    choice = random.choice(['rock','paper','scissors'])
+    return choice
+
+def who_won(player_choice,computer_choice):
+    if player_choice == computer_choice:
+        print(f'Both players chose {}', player_choice)
+    elif player_choice == 'rock' and computer_choice == 'scissors'
